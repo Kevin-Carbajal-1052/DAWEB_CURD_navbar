e625edecdaaaaee9a4b4bdd237fc0c251349d19e
Kevin-Carbajal-1052 <164533993+Kevin-Carbajal-1052@users.noreply.github.com>
2024-11-28 13:45:46 -0600

    Delete productos_app directory

--- productos_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from productos_app import views
-urlpatterns = [
-    path("productos",views.inicio_vistaProductos,name="productos"),
-    path("registrarProductos/",views.registrarProductos,name="registrarProductos"),
-    path("borrarProductos/<idProductos>",views.borrarProductos,name="borrarProductos"),
-    path("seleccionarProductos/<idProductos>",views.seleccionarProductos,name="seleccionarProductos"),
-    path("editarProductos/",views.editarProductos,name="editarProductos")
-]
