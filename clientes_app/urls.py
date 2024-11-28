49b25ea04af5433f95f249c34ced16c402815db4
Kevin-Carbajal-1052 <164533993+Kevin-Carbajal-1052@users.noreply.github.com>
2024-11-28 13:45:28 -0600

    Delete clientes_app directory

--- clientes_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from clientes_app import views
-urlpatterns = [
-    path("clientes",views.inicio_vistaClientes,name="clientes"),
-    path("registrarClientes/",views.registrarClientes,name="registrarClientes"),
-    path("borrarCliente/<idClientes>",views.borrarCliente,name="borrarCliente"),
-    path("seleccionarClientes/<idClientes>",views.seleccionarClientes,name="seleccionarClientes"),
-    path("editarCliente/",views.editarCliente,name="editarCliente")
-]
