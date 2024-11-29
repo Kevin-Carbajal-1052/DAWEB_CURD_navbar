4b8ecb61cfbeb6cc3b5707ce52e1248346556cf6
Kevin-Carbajal-1052 <164533993+Kevin-Carbajal-1052@users.noreply.github.com>
2024-11-29 12:19:53 -0600

    Add files via upload

---
+++ categoria_app/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render,redirect
+from .models import Categoria
+# Create your views here.
+def inicio_vistaCategorias(request):
+    lascategorias=Categoria.objects.all()
+    return render(request, 'gestionarCategoria.html',{'miscategorias':lascategorias})
+
+def registrarCategorias(request):
+    idCategoria=request.POST["idCategoria"]
+    Nombre=request.POST["Nombre"]
+    Promociones=request.POST["Promociones"]
+    Tamaño=request.POST["Tamaño"]
+    Color=request.POST["Color"]
+    Genero=request.POST["Genero"]
+    guardarcategoria=Categoria.objects.create(idCategoria=idCategoria,Nombre=Nombre,Promociones=Promociones,Tamaño=Tamaño,Color=Color,Genero=Genero)
+    return redirect('categorias')
+
+def seleccionarCategorias(request,idCategoria):
+    categoria=Categoria.objects.get(idCategoria=idCategoria)
+    return render(request,"editarCategoria.html",{"miscategorias":categoria})
+
+def editarCategoria(request):
+    idCategoria=request.POST["idCategoria"]
+    Nombre=request.POST["Nombre"]
+    Promociones=request.POST["Promociones"]
+    Tamaño=request.POST["Tamaño"]
+    Color=request.POST["Color"]
+    Genero=request.POST["Genero"]
+    categoria=Categoria.objects.get(idCategoria=idCategoria)
+    categoria.idCategoria=idCategoria
+    categoria.Nombre=Nombre
+    categoria.Promociones=Promociones
+    categoria.Tamaño=Tamaño
+    categoria.Color=Color
+    categoria.Genero=Genero
+    categoria.save()
+    return redirect('categorias')
+
+def borrarCategoria(request,idCategoria):
+    categoria=Categoria.objects.get(idCategoria=idCategoria)
+    categoria.delete()
+    return redirect('categorias')
