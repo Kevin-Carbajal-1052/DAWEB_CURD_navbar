ab5fbbadad9504c517c843af4dd1a83f5100d3ec
Kevin-Carbajal-1052 <164533993+Kevin-Carbajal-1052@users.noreply.github.com>
2024-11-29 12:18:35 -0600

    Delete categoria_app directory

--- categoria_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from categoria_app import views
-urlpatterns = [
-    path("categorias",views.inicio_vistaCategorias,name="categorias"),
-    path("registrarCategorias/",views.registrarCategorias,name="registrarCategorias"),
-    path("borrarCategoria/<idCategoria>",views.borrarCategoria,name="borrarCategoria"),
-    path("seleccionarCategorias/<idCategoria>",views.seleccionarCategorias,name="seleccionarCategorias"),
-    path("editarCategoria/",views.editarCategoria,name="editarCategoria")
-]
